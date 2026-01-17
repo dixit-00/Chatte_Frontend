02fd62d885754956a2a26931e3cc32f47e694c61
dixit-00 <malviyadixit92@gmail.com>
2026-01-17 23:04:52 +0530

    okk

--- components/buttontop.tsx
+++ components/buttontop.tsx
@@ -1,13 +1,1 @@
-import { StyleSheet, Text, View } from "react-native";
 
-const buttontop = () => {
-  return (
-    <View>
-      <Text>buttontop</Text>
-    </View>
-  );
-};
-
-export default buttontop;
-
-const styles = StyleSheet.create({});
